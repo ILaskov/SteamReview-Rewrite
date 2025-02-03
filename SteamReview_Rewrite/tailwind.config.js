1db11e3d66d2a2bbfd9bcf64abfe6f78a51d7d0d
I Laskov <i.laskowski333@gmail.com>
2025-02-03 14:32:10 +0100

    Finally rebased

--- SteamReview_Rewrite/tailwind.config.js
+++ SteamReview_Rewrite/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: [],
   theme: {
     extend: {},
@@ -7,3 +8,13 @@ module.exports = {
   plugins: [],
 }
 
+=======
+    purge: [
+        './**/templates/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+>>>>>>> ef5d983 (Initial commit. Setted up the django project with tailwindcss.)
